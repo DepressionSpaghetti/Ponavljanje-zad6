1a884b3d48ad6a7f8ac7bb2dfd43c389b8a200f3
DepressionSpaghetti <102665381+DepressionSpaghetti@users.noreply.github.com>
2023-09-14 10:08:20 +0200

    Add files via upload

---
+++ Ponavljanje-zad6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponavljanje_zad6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 3, suma = 0;
+            int brojClanova;
+
+            Console.WriteLine("Koliki je broj clanova reda");
+            brojClanova = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0;i<brojClanova;i++)
+            {
+                suma += 1 / a;
+                a *= 3;
+            }
+
+            Console.WriteLine(suma);
+
+
+            Console.ReadKey();
+        }
+    }
+}
